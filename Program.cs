53cc813dcc054f84d05b54dee60e61be112ba67f
Rinazelten <140869746+Rinazelten@users.noreply.github.com>
2023-08-08 10:41:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿/*string numStr = Console.ReadLine(); // ввод данных (предполагается ввод строки)
+int num = int.Parse(num.Str); // Parse - преобразование строки в число
+int result = num * num;
+Console.WriteLine(result);
+*/
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+if ( num1 * num1 == num2)
+{
+    Console.WriteLine("Число 2 является квадратом числа 1");
+}
+else
+{
+    Console.WriteLine("Число 2 не является квадратом числа 1");
+}
+
+
+
+
+Console.WriteLine("Программа показывает наибольшее число из двух введённых");
+Console.WriteLine("Введите первое число 5");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число 7");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (a == b);
+{
+    Console.WriteLine($"Число a = {a} и число b = {b} равны");
+}
+if (a>b);
+{
+    Console.WriteLine($"Число a = {a} больше числа b = {b}");
+}
+if (b>a);
+{
+    Console.WriteLine($"Число b = {b} больше числа a = {a}");
+}
+
+
